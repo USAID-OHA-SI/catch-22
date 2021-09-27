36696ac9f28e93aa7b9fab3d6cca7124a593ece1
tessam30 <tim.essam@gmail.com>
2021-09-27 10:28:37 -0400

    update tables and add footnote to global TX table

--- Scripts/2021_08_31_MDB_tables_FY21Q3_create.R
+++ Scripts/2021_08_31_MDB_tables_FY21Q3_create.R
@@ -22,20 +22,49 @@
     load_secrets()
     merdata <- glamr::si_path("path_msd")
     
-    legend_chunk <- gt::md("Legend: Cumulative Indicators <img src='https://user-images.githubusercontent.com/5873344/131568557-1d2ecf02-bfb8-4f9d-8bff-844871536f92.png' style='height:15px;'>    &emsp; Snapshot (TX_CURR) <img src='https://user-images.githubusercontent.com/5873344/131568585-cef89158-ee77-41a8-9dc7-a3d83ca0ba2c.png' style='height:15px;'>")
+  # Creates a markdown chunk to be inserted as a subtitle with legend pngs
+    legend_chunk <- gt::md(glue::glue("Legend: Cumulative Indicators <img src= '{legend_q3}' style='height:15px;'>    &emsp; Snapshot (TX_CURR) <img src= '{legend_snapshot}' style='height:15px;'> "))
     
-  # Function to add source note
-    add_legend <- function(gt_obj){
+  # Bold Q3 data in the VLS/VLC table
+    bold_column <- function(gt_obj, col){
       gt_obj %>% 
-        tab_source_note(source_note = legend_chunk)
+        tab_style(
+          style = list(
+            gt::cell_fill(color = "#e6e7e8", alpha = 0.5),
+            gt::cell_text(weight = 700)
+          ),
+          locations = cells_body(
+            columns = {{col}},
+          )
+        )
     }
     
-    add_subtitle <- function(gt_obj){
+  # Bold Agency names - used to increase stroke on row group label
+    bold_rowgroup <- function(gt_obj){
       gt_obj %>% 
-        tab_header(
-          title = md("**GLOBAL RESULTS**"),
-          subtitle = legend_chunk
+        tab_style(
+          style = list(
+            gt::cell_text(weight = 700)
+          ),
+          locations = cells_row_groups(groups = everything())
         )
+    }
+    
+  # Generate a list of OUs to loop over 
+    distinct_agg_type <- function(df, type = "OU"){
+      df %>% 
+        filter(agg_type %in% {{type}}) %>% 
+        distinct(operatingunit) %>% 
+        pull()
+    }  
+    
+  # Embiggen font size
+    embiggen <- function(gt_obj){
+      gt_obj %>% 
+        tab_options(
+          source_notes.font.size = 10,
+          table.font.size = 15,
+          footnotes.font.size = 10)
     }
 
 # LOAD DATA ============================================================================  
@@ -43,7 +72,7 @@
     # Load OU_IM table - using FY21 Q2 data from Panorama
     ou_im <- 
       si_path() %>% 
-      return_latest("OU_IM_FY19-21_20210813_v1_1") %>%
+      return_latest("OU_IM_FY19-22_20210917") %>%
       read_msd() 
 
 
@@ -62,50 +91,70 @@
 # VIZ ============================================================================
     
     # Generate base table for global results
-    create_mdb(mdb_tbl, ou = "Global", type = "main", pd, msd_source, legend = legend_chunk) 
+    create_mdb(mdb_tbl, ou = "Global", type = "main", pd, msd_source, legend = legend_chunk) %>% 
+      embiggen
     
     # Try a specific country now
-    create_mdb(mdb_tbl, ou = "Zambia", type = "main", pd, msd_source, legend = legend_chunk) 
+    create_mdb(mdb_tbl, ou = "Zambia", type = "main", pd, msd_source, legend = legend_chunk) %>% embiggen
 
     # Create the treatment data frame needed for derived indicators
     mdb_df_tx    <- make_mdb_tx_df(ou_im)
     mdb_tbl_tx   <- reshape_mdb_tx_df(mdb_df_tx, pd)
     
-    create_mdb(mdb_tbl_tx, ou = "Zambia", type = "treatment", pd, msd_source)
+    create_mdb(mdb_tbl_tx, ou = "Zambia", type = "treatment", pd, msd_source) %>% 
+      bold_column(., Q3) %>% 
+      bold_rowgroup(.) %>% 
+      embiggen()
 
-# BATCH -------------------------------------------------------------------
-
-    # create batch tables
-    distinct_agg_type <- function(df, type = "OU"){
-      df %>% 
-        filter(agg_type == {{type}}) %>% 
-        distinct(operatingunit) %>% 
-        pull()
-    }    
+# BATCH ------------------------------------------------------------------
     
     # Write the different types to character objects
-    ous <- distinct_agg_type(mdb_tbl, "OU")
-    glb <- distinct_agg_type(mdb_tbl, "Agency")
-    rgl <- distinct_agg_type(mdb_tbl, "Region-Country")
+    all_ous <- map(c("OU", "Agency", "Region-Country"), ~distinct_agg_type(mdb_tbl, .x)) %>% 
+      flatten_chr()
     
     # Use purr to map across the list and create tables for all entries in each object
-    purrr::map(list(glb, ous, rgl) %>% flatten(), 
-               ~create_mdb(mdb_tbl, ou = .x, type = "main", pd, msd_source, legend = legend_chunk) %>%
+    purrr::walk(all_ous, 
+               ~create_mdb(mdb_tbl, ou = .x, type = "main", pd, msd_source, legend = legend_chunk) %>% 
+                 bold_rowgroup() %>% 
+                 embiggen() %>% 
                  gtsave(., path = mdb_out, filename = glue::glue("{.x}_{pd}_KEY_INDICATORS_MD.png")))    
     
+        # TREATMENT - generally same as above, but being safe just in case
+    all_tx_ous <- map(c("OU", "Agency", "Region-Country"), ~distinct_agg_type(mdb_tbl_tx, .x)) %>% 
+      flatten_chr()
     
-    # TREATMENT
-    ous_tx <- distinct_agg_type(mdb_tbl_tx, "OU")
-    glb_tx <- distinct_agg_type(mdb_tbl_tx, "Agency")
-    rgl_tx <- distinct_agg_type(mdb_tbl_tx, "Region-Country")
+    purrr::walk(all_tx_ous[29], 
+                ~create_mdb(mdb_tbl_tx, ou = .x, type = "treatment", pd, msd_source) %>%
+                  bold_column(., Q3) %>% 
+                  bold_rowgroup() %>% 
+                  embiggen() %>% 
+                  tab_source_note(
+                    source_note =  "South Africa has no national MMD program and has been excluded from MMD coverage rates.") %>% 
+                  gtsave(., path = mdb_out, filename = glue::glue("{.x}_{pd}_MMD_VL_MD.png")))
     
-    map(list(glb_tx, ous_tx, rgl_tx) %>% flatten(), 
-        ~create_mdb(mdb_tbl_tx, ou = .x, type = "treatment", pd, msd_source) %>% 
-          gtsave(., path = mdb_out, filename = glue::glue("{.x}_{pd}_MMD_VL_MD.png")))
+# EXPORT RAW DATA ------------------------------------------------------------------
 
+  #Export main table and VLC table and migrate to google drive
+   mdb_tbl %>% 
+     select(-c(present_z_direction, present_tint_achv, indicator2)) %>% 
+     rename(FY20_cumulative = past_results_cumulative,
+            FY20_targest = past_targets,
+            FY20_achv = past_targets_achievement,
+            FY21_cumulative = present_results_cumulative,
+            FY21_targets = present_targets,
+            FY21_achv = present_targets_achievement,
+            FY21_results_Q3 = present_z_aresults,
+            FY21_change = present_z_change) %>% 
+     arrange(operatingunit, agency, indicator) %>% 
+     write_csv("Dataout/FY21Q3_core_indicators.csv", na = "")
+   
+   mdb_tbl_tx %>% 
+     select(-c(indicator2, change_dir)) %>% 
+     rename(FY20_results = results,
+            FY21Q1_results = Q1,
+            FY21Q2_results = Q2,
+            FY21Q3_results = Q3) %>% 
+     arrange(operatingunit, agency, indicator) %>% 
+     write_csv("Dataout/FY21Q3_tx_indicators.csv", na = "")
     
-# TODO: Export main table and VLC table and migrate to google drive
-   # Can drop the html formatted columns in each data frame before writing
-   # mdb_tbl %>% write_csv()
-   # mdb_tbl_tx  
     
