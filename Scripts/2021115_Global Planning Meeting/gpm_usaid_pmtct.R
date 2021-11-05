d5d7d189c2f3708a21188d0657dfc59b727cadfd
Karishma Srikanth <krs8rb@virginia.edu>
2021-11-05 16:19:05 -0400

    changed PMTCT viz to by fiscal year instead of quarterly

--- Scripts/2021115_Global Planning Meeting/gpm_usaid_pmtct.R
+++ Scripts/2021115_Global Planning Meeting/gpm_usaid_pmtct.R
@@ -1,8 +1,9 @@
 # PROJECT:  catch-22
 # AUTHOR:   K. Srikanth | USAID
-# PURPOSE:  Global Planning Meeting
+# PURPOSE:  Global Planning Meeting - PMTCT trends for ANC1 and EID <2 months Testing coverage
 # LICENSE:  MIT
 # DATE:     2021-11-1
+# UPDATEDL 2021-11-5
 
 # DEPENDENCIES ------------------------------------------------------------
 
@@ -60,10 +61,11 @@ df_2mo <- df %>%
          ageasentered == "<=02 Months", 
          fiscal_year <= curr_fy) %>%
   group_by(fiscal_year, fundingagency, indicator) %>% 
-  summarise(across(starts_with("qtr"), sum, na.rm = TRUE)) %>% 
+ # summarise(across(starts_with("qtr"), sum, na.rm = TRUE)) %>% 
+  summarise(across(cumulative, sum, na.rm = TRUE)) %>% 
   ungroup() %>% 
-  reshape_msd() %>% 
-  rename(less2mo = value)
+#  reshape_msd() %>% 
+  rename(less2mo = cumulative)
 
 #EID total denominator
 df_denom <- df %>% 
@@ -72,38 +74,48 @@ df_denom <- df %>%
          standardizeddisaggregate == "Total Denominator",
          fiscal_year <= curr_fy) %>% 
   group_by(fiscal_year, fundingagency, indicator) %>% 
-  summarise(across(starts_with("qtr"), sum, na.rm = TRUE)) %>% 
+  #summarise(across(starts_with("qtr"), sum, na.rm = TRUE)) %>% 
+  summarise(across(cumulative, sum, na.rm = TRUE)) %>% 
   ungroup() %>% 
-  reshape_msd() %>% 
-  rename(total_denom = value)
+ # reshape_msd() %>% 
+  rename(total_denom = cumulative)
 
 #join and calculate testing coverage
 df_viz <- df_2mo %>% 
-  left_join(df_denom, by = c("period", "fundingagency", "period_type", "indicator")) %>% 
-  mutate(coverage = less2mo / total_denom)
+  left_join(df_denom, by = c("fiscal_year", "fundingagency", "indicator")) %>% 
+  mutate(coverage = less2mo / total_denom,
+         bar_alpha = ifelse(fiscal_year == 2021, 0.6, 1))
 
 #VIZ -----------------------------------
 
 #grab the latest stat from max period
 latest_stat <- df_viz %>% 
-  filter(period == max(period)) %>% 
-  pull()
+  filter(fiscal_year == max(fiscal_year)) %>% 
+  pull(coverage)
+
+#grab the latest stat from max period
+earliest_stat <- df_viz %>% 
+  filter(fiscal_year == min(fiscal_year)) %>% 
+  pull(coverage)
+
+pct_pt_increase <- (latest_stat - earliest_stat) %>% 
+  percent()
 
 #grab the max period
 latest_pd <- df_viz %>% 
-  slice_max(order_by = period, n = 1) %>% 
-  pull(period)
+  slice_max(order_by = fiscal_year, n = 1) %>% 
+  pull(fiscal_year)
 
 #% testing coverage across periods
 v1 <- df_viz %>% 
-  ggplot(aes(period, coverage, group = period_type)) +
+  ggplot(aes(fiscal_year, coverage)) +
   geom_blank(aes(y = 1.1 * coverage)) +
   geom_line(size = 1.5, color = burnt_sienna_light) +
   geom_point(shape = 21, color = burnt_sienna_light, fill = burnt_sienna_light, size = 12, stroke = 2) +
   geom_text(aes(label = percent(coverage, 1)),
             family = "Source Sans Pro", size = 12/.pt) +
   expand_limits(y = .2) +
-  labs(title = glue("USAID's EID <2 month testing coverage was {percent(latest_stat, 1)} in {latest_pd}"),
+  labs(title = glue("The share of infants receiving their first virologic EID test by <=2 months increased over {pct_pt_increase} points from FY19 to FY21QQ3") %>% toupper(),
        x = NULL, y = NULL) +
   si_style_nolines() +
   theme(axis.text.y = element_blank(),
@@ -113,31 +125,31 @@ v1 <- df_viz %>%
 #Bar chart of # <2 mo EID
 v2 <- df_viz %>%
   mutate(value_label = clean_number(less2mo)) %>% 
-  ggplot(aes(period, less2mo)) + 
+  ggplot(aes(fiscal_year, less2mo)) + 
   #geom_blank(aes(y = 3.5e5)) + 
-  geom_col(aes(fill = burnt_sienna),
+  geom_col(aes(fill = burnt_sienna, alpha = bar_alpha),
            position = "identity") +
   scale_fill_identity() +
-  geom_text(aes(label = value_label), na.rm = TRUE, vjust = -0.5, 
+  scale_alpha_identity() +
+  geom_text(aes(label = value_label), na.rm = TRUE, vjust = 1, 
             family = "Source Sans Pro SemiBold") +
   scale_y_continuous(labels = label_number_si(),
                      position = "right", expand = c(.005, .005)) +
-  #scale_x_continuous(expand = c(.005, .005), n.breaks = unique(df_kp_viz$period) %>% length())+
   #scale_alpha_identity() +
   #scale_fill_manual(values = c(denim, denim_light)) +
   labs(x = NULL, y = NULL, fill = NULL,
-       caption = glue("Source: {msd_source} (including FY15-18)
-                        SI analytics: {paste(authors, collapse = '/')}
-                     US Agency for International Development")) +
+       caption = glue("Source: {msd_source} (including FY15-18)",
+                      "USAID SI Analytics",
+                      "Global Planning Meeting 2021-11-15", .sep = " | ")) +
   si_style_nolines() +
   theme(legend.position = "none",
-        strip.text.x = element_text(family = "Source Sans Pro SemiBold", size = 13),
-        panel.spacing.x = unit(1, "lines"),
+        #strip.text.x = element_text(family = "Source Sans Pro SemiBold", size = 13),
+        #panel.spacing.x = unit(1, "lines"),
         panel.spacing.y = unit(.5, "lines")) + 
   coord_cartesian(expand = F, clip = "off")
 
 #patchwork together
 (v1/v2) +
-  plot_layout(heights = c(3, 8))
+  plot_layout(heights = c(3, 6))
 
-si_save("GPM_PMTCT.svg") 
+si_save("Graphics/GPM_PMTCT.svg") 
