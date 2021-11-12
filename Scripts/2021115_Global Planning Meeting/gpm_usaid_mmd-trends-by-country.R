cea53fdaec8886a1e18208bb2be0eac65f30f6dd
Aaron Chafetz <achafetz@usaid.gov>
2021-11-12 14:11:07 -0500

    update plots for asia region

--- Scripts/2021115_Global Planning Meeting/gpm_usaid_mmd-trends-by-country.R
+++ Scripts/2021115_Global Planning Meeting/gpm_usaid_mmd-trends-by-country.R
@@ -3,7 +3,7 @@
 # PURPOSE:  MMD by OU visual for FY21Q3 review
 # LICENSE:  MIT
 # DATE:     2021-11-09
-# UPDATED: 
+# UPDATED:  2021-11-12
 # NOTE:     based on groundhogday/FY21Q3-Review_MMD-trends.R
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -58,15 +58,19 @@
   #add in agency agg and reshape
   df_mmd <- df_mmd %>%
     bind_rows(df_mmd  %>%
-                mutate(countryname = "USAID")) %>% 
-    group_by(fiscal_year, countryname, indicator, otherdisaggregate) %>% 
+                mutate(operatingunit = "USAID",
+                       countryname = "USAID")) %>% 
+    group_by(fiscal_year, operatingunit, countryname, indicator, otherdisaggregate) %>% 
     summarise(across(starts_with("qtr"), sum, na.rm = TRUE), .groups = "drop") %>% 
     reshape_msd() %>% 
     filter(value > 0)
   
   #create group for o3mo and o6mo via reshaping for plotting
   df_mmd <- df_mmd %>% 
-    mutate(countryname = recode(countryname,
+    mutate(operatingunit = recode(operatingunit,
+                                "Democratic Republic of the Congo" = "DRC",
+                                "Dominican Republic" = "DR"),
+           countryname = recode(countryname,
                                 "Democratic Republic of the Congo" = "DRC",
                                 "Dominican Republic" = "DR")) %>% 
     select(-period_type) %>% 
@@ -79,7 +83,7 @@
     ungroup() %>% 
     rename(o6mmd = `6 or more months`) %>% 
     select(-`Less than 3 months`, -`3 to 5 months`) %>% 
-    pivot_longer(-c(period, countryname, indicator, total), 
+    pivot_longer(-c(period, operatingunit, countryname, indicator, total), 
                  names_to = "otherdisaggregate",
                  values_to = "tx_mmd") %>% 
     rename(tx_curr = total) 
@@ -106,7 +110,8 @@
     ungroup() %>% 
     mutate(country_lab = case_when(max_tx == max(max_tx) ~ 
                                      glue("{countryname}<br><span style = 'font-size:8pt'>{clean_number(max_mmd)} / {clean_number(max_tx)} <span style = 'font-size:6pt'>(+6 MMD/TX_CURR)</span>"),
-                                   TRUE ~ glue("{countryname}<br><span style = 'font-size:8pt'>{clean_number(max_mmd)} / {clean_number(max_tx)}</span>"))) %>% 
+                                   TRUE ~ glue("{countryname}<br><span style = 'font-size:8pt'>{clean_number(max_mmd)} / {clean_number(max_tx)}</span>")),
+           country_lab = str_replace(country_lab, "NA", "0")) %>% 
     filter(max_tx > 0)
 
   
@@ -138,14 +143,19 @@
     slice_max(order_by = tx_curr, n = top$n + 1) %>% 
     pull(countryname)
   
+  #asia region
+  xar_cntry <- df_mmd_ou %>% 
+    filter(operatingunit %in% c("Asia Region", "Vietnam","USAID"),
+           otherdisaggregate == "o6mmd",
+           period == max(period)) %>% 
+    pull(countryname)
+  
 # VIZ ---------------------------------------------------------------------
 
   df_mmd_ou %>%
     filter(countryname %in% top_cntry) %>%
     ggplot(aes(period, share, group = otherdisaggregate, color = fill_color, fill = fill_color)) +
-    geom_area(alpha = .4, 
-              size = .9, position = "identity"
-              ) +
+    geom_area(alpha = .4, size = .9, position = "identity") +
     geom_point(aes(y = endpoints), na.rm = TRUE) +
     facet_wrap(~fct_reorder2(country_lab, period, tx_curr, .desc = TRUE)) +
     scale_y_continuous(label = percent, 
@@ -169,4 +179,34 @@
           strip.text = element_markdown())    
 
   si_save("Graphics/gpm_usaid_mmd-trends-by-country.svg", height = 4.25)    
+  
+  
+  df_mmd_ou %>%
+    filter(countryname %in% xar_cntry) %>%
+    ggplot(aes(period, share, group = otherdisaggregate, color = fill_color, fill = fill_color)) +
+    geom_area(alpha = .4, size = .9, position = "identity") +
+    geom_point(aes(y = endpoints), na.rm = TRUE) +
+    facet_wrap(~fct_reorder2(country_lab, period, tx_curr, .desc = TRUE)) +
+    scale_y_continuous(label = percent, 
+                       breaks = seq(0, 1, .5)) +
+    scale_x_discrete(breaks = c("FY20Q1", "FY20Q3", "FY21Q1", "FY21Q3")) +
+    scale_color_identity(aesthetics = c("color","fill")) +
+    expand_limits(y = 1) +
+    labs(x = NULL, y = NULL,
+         title = glue("USAID HAS MADE LIMITED GAINS TOWARDS GETTING TREATMENT PATIENTS ON +6 MONTHS OF MMD SINCE FY20Q3 ESPECIALLY IN ASIA REGION"),
+         subtitle = "South Africa, representing a third of USAID's treatment portfolio, has been excluded from USAID total",
+         caption = glue("MMD 3 months or more = 3-5 months and 6 months or more",  
+                        "Source: {msd_source}",
+                        "USAID SI Analytics",
+                        "Global Planning Meeting 2021-11-15", .sep = " | ")) +
+    si_style_ygrid() +
+    theme(panel.spacing.y = unit(.5, "line"),
+          panel.spacing.x = unit(.5, "line"),
+          axis.text.x = element_text(size = 9),
+          axis.text.y = element_text(size = 7),
+          panel.grid.major.y = element_line(color = "#E8E8E8"),
+          panel.grid.minor.y = element_line(color = "#E8E8E8"),
+          strip.text = element_markdown())  
+  
+  si_save("Graphics/gpm_usaid_mmd-trends-by-country_asia.svg", height = 4.25) 
   
---
+++ Scripts/2021115_Global Planning Meeting/gpm_usaid_vlc-by-country-asia.R
@@ -0,0 +1,183 @@
+# PROJECT:  catch-22
+# AUTHOR:   A.Chafetz, T.Essam, K.Srikanth | USAID
+# PURPOSE:  Explore VLS aross USAID mechanisms for Asia Region
+# LICENSE:  MIT
+# DATE:     2021-10-18
+# UPDATED:  2021-11-12
+# NOTE:     derived from catch-22/20211018_USAID_VLC-by-country.R
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  library(tidyverse)
+  library(glitr)
+  library(glamr)
+  library(gophr)
+  library(extrafont)
+  library(scales)
+  library(tidytext)
+  library(patchwork)
+  library(ggtext)
+  library(glue)
+  library(ggrepel)
+  
+
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  msd_source <- source_info()
+  pd <- source_info(return = "period")
+
+  authors <- c("Aaron Chafetz", "Tim Essam", "Karishma Srikanth")
+  
+  #site adjusted dataset
+  site_adj_path <- "../right_size/Dataout/TX_CURR_NN_Calcs.csv"
+  
+# IMPORT ------------------------------------------------------------------
+  
+  df_siteadj <- read_csv(site_adj_path)
+
+# MUNGE SITE ADJUSTMENT ---------------------------------------------------
+
+  df_vls_adj <- df_siteadj %>% 
+    filter(fundingagency == "USAID",
+           period == pd)
+  
+  df_vls_adj <- df_vls_adj %>% 
+    bind_rows(df_vls_adj %>% 
+                mutate(across(c(mech_code, mech_name, primepartner), ~ "National"))) %>% 
+    mutate(type = ifelse(mech_code == "National", "national", "mech")) %>% 
+    group_by(operatingunit, countryname, type, mech_code, mech_name, primepartner) %>% 
+    summarise(across(c(tx_curr, tx_curr_lag2 = tx_curr_lag2_site, tx_pvls, tx_pvls_d), 
+                     sum, na.rm = TRUE), .groups = "drop") %>% 
+    filter(tx_pvls_d > 0)
+  
+  
+  #calc VLC/S
+  df_vls_adj <- df_vls_adj %>% 
+    mutate(vlc = tx_pvls_d/tx_curr_lag2,
+           vls = tx_pvls/tx_pvls_d,
+           vls_alt = tx_pvls/tx_curr_lag2,
+           vls_goal_gap = round(.9*tx_pvls_d, 0) - tx_pvls,
+           vls_goal_gap = ifelse(vls_goal_gap < 0, 0, vls_goal_gap))
+  
+  
+  # #identify where 80% of TX_CURR is for viz facet
+  # df_ctry_grps_adj <- df_vls_adj %>% 
+  #   filter(mech_code == "National") %>% 
+  #   count(operatingunit, wt = tx_curr) %>% 
+  #   arrange(desc(n)) %>% 
+  #   mutate(cumsum = cumsum(n),
+  #          share = cumsum/sum(n),
+  #          grp = case_when(share < .83 ~ "Top 80%",
+  #                          share < .95 ~ "Next 15%",
+  #                          TRUE ~ "Remaining 5%")) %>% 
+  #   select(operatingunit, grp)
+  # 
+  
+
+# MUNGE SITE ADJUSTMENT ---------------------------------------------------
+  
+  df_vlc_adj <- df_siteadj %>% 
+    filter(fundingagency == "USAID",
+           period == pd,
+           vlc_valid == TRUE)
+  
+  df_vlc_adj <- df_vlc_adj %>% 
+    bind_rows(df_vlc_adj %>% 
+                mutate(across(c(mech_code, mech_name, primepartner), ~ "National"))) %>% 
+    mutate(type = ifelse(mech_code == "National", "national", "mech")) %>% 
+    group_by(operatingunit, countryname, type, mech_code, mech_name, primepartner) %>% 
+    summarise(across(c(tx_curr, tx_curr_lag2 = tx_curr_lag2_site, tx_pvls, tx_pvls_d), 
+                     sum, na.rm = TRUE), .groups = "drop") %>% 
+    filter(tx_pvls_d > 0)
+  
+  
+  #calc VLC/S
+  df_vlc_adj <- df_vlc_adj %>% 
+    mutate(vlc = tx_pvls_d/tx_curr_lag2,
+           vls = tx_pvls/tx_pvls_d,
+           vls_alt = tx_pvls/tx_curr_lag2,
+           vls_goal_gap = round(.9*tx_pvls_d, 0) - tx_pvls,
+           vls_goal_gap = ifelse(vls_goal_gap < 0, 0, vls_goal_gap))
+  
+  df_usaid_vlc_adj <- df_vlc_adj %>% 
+    filter(type == "national") %>%
+    summarise(across(starts_with("tx"), sum, na.rm = TRUE)) %>% 
+    mutate(vlc = tx_pvls_d/tx_curr_lag2,
+           vls = tx_pvls/tx_pvls_d,
+           vls_alt = tx_pvls/tx_curr_lag2,
+           vls_goal_gap = round(.9*tx_pvls_d, 0) - tx_pvls,
+           vls_goal_gap = ifelse(vls_goal_gap < 0, 0, vls_goal_gap))
+  
+  #viz dataframe
+  df_viz_vlc <- df_vlc_adj %>% 
+    # left_join(df_ctry_grps_adj, by = "operatingunit") %>% 
+    mutate(vls_mech = case_when(mech_code != "National" ~ vls_alt),
+           vls_mech = ifelse(vls_mech > 1.01, 1.01, vls_mech),
+           vls_nat = case_when(mech_code == "National" ~ vls_alt),
+           vlc_mech = case_when(mech_code != "National" ~ vlc),
+           vlc_mech = ifelse(vlc_mech > 1.1, 1.1, vlc_mech),
+           vlc_nat = case_when(mech_code == "National" ~ vlc),
+           fill_color = ifelse(vlc_mech > .9, scooter, moody_blue),
+           # operatingunit = recode(operatingunit, 
+           #                      "Democratic Republic of the Congo" = "DRC",
+           #                      "Papua New Guinea" = "PNG",
+           #                      "Dominican Republic" = "DR",
+           #                      "West Africa Region" = "WAR",
+           #                      "Western Hemisphere Region" = "WHR"),
+           clean_name = case_when(str_detect(mech_name, "EpiC") ~ "EpiC",
+                                  primepartner == "Abt Associates Inc." ~ "Abt",
+                                  primepartner == "Family Health International" ~ "FHI360",
+                                  str_detect(primepartner, "INTRAHEALTH") ~ "IntraHealth",
+                                  str_detect(primepartner, "HEALTH THROUGH") ~ "Health Through Walls",
+                                  primepartner == "Caris Foundation International" ~ "Caris Foundation",
+                                  primepartner == "Johns Hopkins University, The" ~ "JHU",
+                                  primepartner == "JSI Research And Training Institute, INC." ~ "JSI",
+                                  primepartner == "PAKACHERE INSTITUTE OF HEALTH AND DEVELOPMENT COMMUNICATION" ~ "Pakachere",
+                                  primepartner == "WITS HEALTH CONSORTIUM (PTY) LTD" ~ "Wits",
+                                  TRUE ~ str_to_title(primepartner)),
+           # grp = factor(grp, c("Top 80%", "Next 15%", "Remaining 5%")),
+           mech_lab = case_when(vlc_mech < .9 ~ glue("{clean_name})")))
+  
+  df_viz_vlc_asia <- df_viz_vlc %>% 
+    filter(operatingunit %in% c("Asia Region", "Vietnam"))
+    
+  df_viz_vlc_asia %>% 
+    ggplot(aes(vlc_mech, fct_reorder(countryname, vlc_nat, na.rm = TRUE))) +
+    geom_blank() +
+    annotate("rect",
+             xmin = -Inf, xmax = .9, ymin = 0, ymax = Inf,
+             fill = trolley_grey_light, alpha = .4) +
+    geom_vline(xintercept = .9, linetype = "dashed") +
+    geom_point(aes(size = tx_curr, 
+                   color = fill_color), alpha = .6,
+               # position = position_jitter(width = 0, height = 0.1, seed = 42), 
+               na.rm = TRUE) +
+    # geom_text_repel(aes(label = mech_lab), na.rm = TRUE, max.overlaps = 30,
+    #                 family = "Source Sans Pro", color = "#505050", size = 9/.pt) +
+    # geom_errorbar(aes(xmin = vlc_nat, xmax = vlc_nat), size = 1.1, color = burnt_sienna) + #grey60k
+    scale_x_continuous(label = percent_format(1)) +
+    # facet_grid(grp ~ ., scale = "free_y", space = "free") +
+    scale_size(labels = number_format(1, scale = 1e-3, suffix = "K"),
+               range = c(2,10)) +
+    scale_color_identity() +
+    coord_cartesian(clip = "off") +
+    expand_limits(x = .75) +
+    labs(y = NULL, x = "Viral Load Coverage Rate (TX_PVLS_D/TX_CURR [2 Qtrs Prior])",
+         title = glue("With USAID's VLC Rate at {percent(df_usaid_vlc_adj$vlc, 1)} in {pd}, the Agency has significant work to reach the goal of 90% VLC") %>% toupper(),
+         size = glue("Current on Treatment ({pd})"),
+         
+         caption = glue("Note: Adjusted site TX_CURR data account for site transitions & exclude non-eligible VLC calculations
+                        Source: Site Adjusted DATIM Pull [2021-10-05]",
+                        "USAID SI Analytics",
+                        "Global Planning Meeting 2021-11-15", .sep = " | ")) +
+    si_style(facet_space = .5) +
+    theme(#legend.position = "none",
+      axis.text.y = element_text(size = 9),
+      strip.text.y = element_text(hjust = .5, family = "Source Sans Pro SemiBold"))  
+  
+  
+  si_save("Graphics/gpm_usaid_vlc_asia.svg",
+          height = 4.25)
+  
+  
+  
