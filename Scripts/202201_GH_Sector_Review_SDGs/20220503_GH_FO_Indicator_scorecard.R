42bc5454e9a9c4f3733d7dc6cfcdd98986dcc6f0
Karishma Srikanth <krs8rb@virginia.edu>
2022-06-23 11:50:18 -0400

    adding life expectancy estimates from WPP 2019 and addressing country name issues

--- Scripts/202201_GH_Sector_Review_SDGs/20220503_GH_FO_Indicator_scorecard.R
+++ Scripts/202201_GH_Sector_Review_SDGs/20220503_GH_FO_Indicator_scorecard.R
@@ -127,6 +127,11 @@ df_mal_deaths <- si_path() %>%
   return_latest("malaria-death-rates.csv") %>% 
   read_csv()
 
+#Life expectancy - WPP 2019
+df_wpp <- read_xlsx("Data/WPP2019_SA1_INT_F01_ANNUAL_DEMOGRAPHIC_INDICATORS.xlsx",
+                    sheet = "ESTIMATES",
+                    skip = 16) %>% 
+  clean_names()
 
 #MASTER LIST - region meta data
 google_id <- "14sBgteFOvPfQi5lB29334zNO_6aP4VPsVinhNTYUFa8"
@@ -156,7 +161,18 @@ munge_uhc <- function(df) {
            year = period) %>% 
    # filter(year == max(year)) %>% 
     mutate(country = recode(country, "United Republic of Tanzania" = "Tanzania",
-                            "Democratic Republic of the Congo" = "Democratic Republic of Congo"),
+                           # "Democratic Republic of the Congo" = "Democratic Republic of Congo",
+                            "Viet Nam" = "Vietnam",
+                            "Bolivia (Plurinational State of)" = "Bolivia",
+                            "Myanmar" = "Burma",
+                            "Lao People's Democratic Republic" = "Laos",
+                            "The former Yugoslav Republic of Macedonia" = "North Macedonia",
+                            "Venezuela (Bolivarian Republic of)" = "Venezuela",
+                            "Côte d’Ivoire" = "Cote d'Ivoire",
+                            "Czechia" = "Czech Republic",
+                           "United Kingdom of Great Britain and Northern Ireland" = "United Kingdom",
+                           "Brunei Darussalam" = "Brunei"
+                            ),
       usaid = ifelse(country %in% usaid_mch, "USAID MCHN", "Non-USAID MCHN"))
   
   return(df)
@@ -173,6 +189,18 @@ sdg_reshape <- function(data) {
   
   data_clean <- data_clean %>% 
     mutate(indicator = indic_name,
+           country = recode(country, "Myanmar" = "Burma",
+                                   "Timor" = "Timor-Leste",
+                            "North Korea" = "Democratic People's Republic of Korea",
+                            "South Korea" = "Republic of Korea",
+                            "Moldova" = "Republic of Moldova",
+                            "Russia" = "Russian Federation",
+                            "Iran" = "Iran (Islamic Republic of)",
+                            "Syria" = "Syrian Arab Republic",
+                            "Democratic Republic of Congo" = "Democratic Republic of the Congo",
+                            "Cape Verde" = "Cabo Verde",
+                            "Czechia" = "Czech Republic",
+                            "United States" = "United States of America"),
            indicator = recode(indicator, "maternal_mortality_ratio_modeled_estimate_per_100_000_live_births" = "maternal_mortality",
                               "mortality_rate_neonatal_per_1_000_live_births" = "neonatal_mortality",
                               "mortality_rate_under_5_per_1_000_live_births" = "under5_mortality",
@@ -210,6 +238,12 @@ df_uhc_sub1 <- munge_uhc(df_uhc_sub1)
 df_uhc_sub2 <- munge_uhc(df_uhc_sub2)
 df_uhc_sub3 <- munge_uhc(df_uhc_sub3)
 df_uhc_sub4 <- munge_uhc(df_uhc_sub4)
+
+df_tt <- df_tt %>% 
+  mutate(country = recode(country, "Democratic People Republic of Korea" = "Democratic People's Republic of Korea",
+                          "Cape Verde" = "Cabo Verde",
+                          "Brunei Darussalam"  = "Brunei")) %>% 
+  filter(country != regions)
 
 df_uhc_final <- df_uhc %>% 
   bind_rows(df_uhc_sub1, df_uhc_sub2, df_uhc_sub3, df_uhc_sub4) %>% 
@@ -263,14 +297,63 @@ df_vls <- df_tt %>%
     ref_link = "https://aidsinfo.unaids.org/",
          date_data_pulled = lubridate::today())
 
+#clean up life expectancy estimates
+df_wpp <- df_wpp %>% 
+  rename(country = region_subregion_country_or_area,
+         year = reference_date_1_january_31_december) %>% 
+  select(country, country_code, type, parent_code, year, starts_with('life_expectancy_at_birth_both')) %>% 
+  pivot_longer(c(starts_with("life")), names_to = "indicator", values_to = "value") %>% 
+  mutate(value = as.numeric(value),
+         country = recode(country, "Samoa" = "American Samoa",
+                          "Bolivia (Plurinational State of)" = "Bolivia",
+                          "Brunei Darussalam" = "Brunei",
+                          "Myanmar" = "Burma",
+                          "Côte d'Ivoire" = "Cote d'Ivoire",
+                          "Curaçao" = "Curacao",
+                          "Czechia" = "Czech Republic",
+                          "Dem. People's Republic of Korea" = "Democratic People's Republic of Korea",
+                          "China, Hong Kong SAR" = "Hong Kong",
+                          "Lao People's Democratic Republic" = "Laos",
+                          "China, Macao SAR" = "Macao",
+                          "Micronesia (Fed. States of)" = "Micronesia (country)",
+                          "State of Palestine" = "Palestine",
+                          "China, Taiwan Province of China" = "Taiwan",
+                          "United Republic of Tanzania" = "Tanzania",
+                          "Venezuela (Bolivarian Republic of)" = "Venezuela",
+                          "Viet Nam" = "Vietnam"),
+         indicator = recode(indicator, "life_expectancy_at_birth_both_sexes_combined_years" = "life_expectancy_at_birth_all_sexes")) %>% 
+  filter(type == "Country/Area") 
 
-#FINAL DF --------------------------------------------------------------
+
+
+
+#FINAL DF --------------------------------------------------------------'
+
+#bind all the data together
 df_gh_final <- df_uhc_final %>% 
   bind_rows(df_under5_clean) %>% 
   bind_rows(df_tb_clean) %>% 
   bind_rows(df_mal_clean) %>% 
   rename(cntry_group = usaid) %>% 
   bind_rows(df_vls) %>% 
+  arrange(country) %>% 
+  filter(country != "World")
+
+#join ISOs for WPP
+country_iso <- df_gh_final %>% count(country, iso) %>% select(-(n))
+
+df_wpp_clean <- df_wpp %>% 
+  left_join(country_iso, by = c("country")) %>% 
+  mutate(iso = case_when(country == "Western Sahara" ~ "ESH",
+                         TRUE ~ iso)) %>% 
+  mutate(cntry_group = NA,
+         goal = NA,
+         ref_link = "https://population.un.org/wpp/Download/SpecialAggregates/EconomicTrading/",
+         date_data_pulled = lubridate::today()) %>% 
+  select(indicator, iso, country, year, value, cntry_group, ref_link, date_data_pulled)
+
+df_gh_final <- df_gh_final %>%
+  bind_rows(df_wpp_clean) %>% 
   arrange(country) %>% 
   left_join(region_meta
             %>% dplyr::select(iso, usaid_supported, usaid_region, idea_region, income_group)
