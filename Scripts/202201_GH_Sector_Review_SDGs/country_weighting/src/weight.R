a4deecd922d95f3280c1d286b746fdcc31f5fd52
jhoehner <jessmhoehner@gmail.com>
2022-07-19 17:45:47 -0400

    WIP- updated after convo with MD and KS

--- Scripts/202201_GH_Sector_Review_SDGs/country_weighting/src/weight.R
+++ Scripts/202201_GH_Sector_Review_SDGs/country_weighting/src/weight.R
@@ -27,8 +27,6 @@
   library(assertthat)
   library(ggtext)
 
-  extrafont::loadfonts(device = "win")
-
 # global variables -------------------------------------------------------------
 
 ref_id <- "ae3887aa"
@@ -44,6 +42,7 @@ inputs <- list(
 outputs <- list(
   hsc_data = here("catch-22/Scripts/202201_GH_Sector_Review_SDGs/country_weighting/output/hsc_weightedavgdata.csv"),
   hsc_fig = here("catch-22/Scripts/202201_GH_Sector_Review_SDGs/country_weighting/output/hsc_weightedavgfig.svg"),
+  hsc_fig2 = here("catch-22/Scripts/202201_GH_Sector_Review_SDGs/country_weighting/output/hsc_weightedavgdomainfig.svg"),
   lifeexp_data = here("catch-22/Scripts/202201_GH_Sector_Review_SDGs/country_weighting/output/lifeexp_weightedavgdata.csv"),
   lifeexp_fig = here("catch-22/Scripts/202201_GH_Sector_Review_SDGs/country_weighting/output/lifeexp_weightedavgfig.svg"))
 
@@ -60,38 +59,56 @@ names <- read_csv(inputs$country_names,
 selected_data <- read_csv(inputs$unweighted_data,
   show_col_types = FALSE) %>%
   clean_names() %>%
-  select(country, iso, indicator, usaid_supported, year, value) %>%
+  select(idea_region, usaid_region, country, iso,
+         usaid_supported, income_group, indicator,
+         year, value, date_data_pulled, ref_link, cntry_group) %>%
+  # keep only life expectancy and UHC indicators
   filter(indicator %in% c(
     "uhc_service_coverage_index",
-    "life_expectancy_at_birth_all_sexes")) %>%
+    "life_expectancy_at_birth_all_sexes",
+    "uhc_subindex1_capacity_access",
+    "uhc_subindex2_ncd",
+    "uhc_subindex3_mchn",
+    "uhc_subindex4_id")) %>%
   # filter rows with no usaid_supported data
   drop_na(usaid_supported) %>%
   # join with GH country list names
   left_join(., names,
     by = c("country" = "country_unweighted")) %>%
   mutate(
-    across(.cols = country:indicator, ~ as.character(.)),
+    across(.cols = idea_region:indicator, ~ as.character(.)),
     across(.cols = year:value, ~ as.numeric(.)),
     usaid_supported = as_factor(usaid_supported),
     # replace country names from unweighted data with names from GH list
     # provided by Karishma, otherwise keep the country name
     country = as.character(if_else(is.na(country_ghlist) == FALSE,
       country_ghlist, country))) %>%
-  select(country, iso, indicator, usaid_supported, year, value) %>%
+  select(idea_region, usaid_region, country, iso,
+         usaid_supported, income_group, indicator,
+         year, value, date_data_pulled, ref_link, cntry_group) %>%
   distinct()
 
 # What percentage of the data includes countries supported by USAID?
-
-countries_by_support <- selected_data %>%
-  select(country, usaid_supported) %>%
-  group_by(usaid_supported, country) %>%
+country_by_support <- selected_data %>%
+  select(country, usaid_supported, income_group) %>%
+  group_by(country, usaid_supported, income_group) %>%
   distinct()
 
-tabyl(countries_by_support$usaid_supported)
+countries_by_support <- tabyl(country_by_support$usaid_supported)
 
-# ~97% (0.97) of countries in this dataset (66) are supported by USAID
-# and 2 are not. It seems like we have class imbalance here. Is this an
+# ~72% (0.72) of countries in this dataset (108) are supported by USAID
+# and 42 are not. It seems like we have class imbalance here. Is this an
 # issue when only describing the data?
+
+# of the countries in each income group, how many are USAID supported?
+countries_by_support_income <- tabyl(country_by_support, usaid_supported, income_group)
+
+# among high income countries, 6 are USAID supported (19 are not)
+# among UMI countries 35 are USAID supported (13 are not)
+# among low income countries 23 are USAID supported and 2 are not
+# among LMI countries 43 are USAID supported, 8 are not
+
+# These are very imbalanced...
 
 pop_data <- world_pop %>%
   pivot_longer(
@@ -123,41 +140,70 @@ full_data <- selected_data %>%
   # (pre 1960, will work back in later)
   drop_na(population) %>%
   distinct() %>%
-  group_by(country, indicator, usaid_supported, year) %>%
+  select(idea_region, usaid_region, country, iso,
+         usaid_supported, income_group, indicator,
+         year, value, population, date_data_pulled,
+         ref_link, cntry_group) %>%
+  group_by(idea_region, usaid_region, country, iso,
+           usaid_supported, income_group,
+           indicator, year) %>%
   mutate(
     value = as.numeric(value),
     population = as.numeric(population),
-    weight = as.numeric(sum(value * population)))
+    weight = as.numeric(sum(value*population))) %>%
+  distinct()
 
 # HSC index in USAID vs non-USAID countries
-
-hsc <- full_data %>%
-  filter(indicator == "uhc_service_coverage_index") %>%
-  ungroup() %>%
-  select(usaid_supported, year, value, weight) %>%
+# annual population weighted by USAID support
+uhc_low_support <- full_data %>%
+  filter(indicator == "uhc_service_coverage_index",
+         income_group == "Low Income Country (World Bank Classification)") %>%
   group_by(usaid_supported, year) %>%
-  summarize(value = value,
-            weight = weight,
+  summarize(idea_region = idea_region,
+            usaid_region = usaid_region,
+            country = country,
+            iso = iso,
+            usaid_supported = usaid_supported,
+            income_group = income_group,
+            indicator = indicator,
+            year = year,
+            value = value,
             unweighted_avg = round_half_up(mean(value), 2),
-            weighted_avg = round_half_up(weighted.mean(value, weight), 2))
+            weighted_avg = round_half_up(weighted.mean(value, weight), 2),
+            population = population,
+            date_data_pulled = date_data_pulled,
+            ref_link = ref_link,
+            cntry_group = cntry_group) %>%
+  distinct()
 
 # Life Expectancy at Birth in USAID vs non-USAID countries
-
-life_exp <- full_data %>%
-  filter(indicator == "life_expectancy_at_birth_all_sexes") %>%
-  ungroup() %>%
-  select(usaid_supported, year, value, weight) %>%
+# annual population weighted by USAID support
+lifeexp_low_support <- full_data %>%
+  filter(indicator == "life_expectancy_at_birth_all_sexes",
+         income_group == "Low Income Country (World Bank Classification)") %>%
   group_by(usaid_supported, year) %>%
-  summarize(value = value,
-            weight = weight,
+  summarize(idea_region = idea_region,
+            usaid_region = usaid_region,
+            country = country,
+            iso = iso,
+            usaid_supported = usaid_supported,
+            income_group = income_group,
+            indicator = indicator,
+            year = year,
+            value = value,
             unweighted_avg = round_half_up(mean(value), 2),
-            weighted_avg = round_half_up(weighted.mean(value, weight), 2))
+            weighted_avg = round_half_up(weighted.mean(value, weight), 2),
+            population = population,
+            date_data_pulled = date_data_pulled,
+            ref_link = ref_link,
+            cntry_group = cntry_group) %>%
+  distinct()
 
 # visualize --------------------------------------------------------------------
 
 # What has been the change in HSC index in USAID vs non-USAID countries over time?
-
-ggplot(hsc, aes(
+# filter by lower income
+ggplot(uhc_low_support, aes(
   x = year,
   y = weighted_avg,
   group = usaid_supported,
@@ -174,25 +220,25 @@ ggplot(hsc, aes(
       "Yes" = usaid_blue,
       "No" = usaid_darkgrey),
     labels = NULL) +
-  theme(axis.text.x = element_text(size = 10,
-                                   color = "#505050"),
-        axis.text.y = element_text(size = 10,
-                                   color = "#505050"),
-        plot.title = element_markdown(size = 14,
+  theme( axis.text = element_text(family = "Source Sans Pro",
+                                  size = 10,
+                                  color = "#505050"),
+        plot.title = element_markdown(family = "Source Sans Pro",
+                                      size = 14,
                                       color = "#202020"),
         legend.position = "none") +
   labs(
     x = NULL,
     y = NULL,
     color = NULL,
-    title = "GAINS IN <b>POPULATION WEIGHTED HEALTH SERVICE COVERAGE INDEX SINCE 2020
+    title = "GAINS IN <b>POPULATION WEIGHTED HEALTH SERVICE COVERAGE INDEX</b> SINCE 2020
             <span style='color: #002a6c;'>USAID</span> AND <span style='color: #6c6463;'>NON-USAID</span> LOWER-INCOME COUNTRIES")
 
 si_save(outputs$hsc_fig)
 
 # What has been the change in Life Expectancy at Birth in USAID vs non-USAID countries over time?
-
-ggplot(life_exp, aes(
+# filter by lower income
+ggplot(lifeexp_low_support, aes(
   x = year,
   y = weighted_avg,
   group = usaid_supported,
@@ -211,25 +257,26 @@ ggplot(life_exp, aes(
       "Yes" = usaid_blue,
       "No" = usaid_darkgrey),
     labels = NULL) +
-  theme(axis.text.x = element_text(size = 10,
-                                   color = "#505050"),
-        axis.text.y = element_text(size = 10,
-                                   color = "#505050"),
-        plot.title = element_markdown(size = 14,
+  theme(axis.text = element_text(family = "Source Sans Pro",
+                                  size = 10,
+                                  color = "#505050"),
+        plot.title = element_markdown(family = "Source Sans Pro",
+                                      size = 14,
                                       color = "#202020"),
         legend.position = "none") +
   labs(
     x = NULL,
     y = NULL,
     color = NULL,
-    title = "INCREASES IN <b>POPULATION WEIGHTED LIFE EXPECTANCY AT BIRTH SINCE 1960
+    title = "INCREASES IN <b>POPULATION WEIGHTED LIFE EXPECTANCY AT BIRTH</b> SINCE 1960
              <span style='color: #002a6c;'>USAID</span> AND <span style='color: #6c6463;'>NON-USAID</span> LOWER-INCOME COUNTRIES")
 
 si_save(outputs$lifeexp_fig)
 
 # save data --------------------------------------------------------------------
 
-write_excel_csv(hsc, outputs$hsc_data)
-write_excel_csv(life_exp, outputs$lifeexp_data)
+# add full data set here
+write_excel_csv(hsc_popsup, outputs$hsc_data)
+write_excel_csv(life_exp_popsup, outputs$lifeexp_data)
 
 # end
