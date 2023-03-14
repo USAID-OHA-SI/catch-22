0f803eaeeb6cc6508b2eae82df28eb52b663860c
Prasann R <prasann@gwu.edu>
2023-03-14 17:42:54 -0400

    Revise previous FO TX script
    Correcting numbers inconsistency with MSD and Pano

--- Scripts/20230310_FO_Historic_TX_trends.R
+++ Scripts/20230310_FO_Historic_TX_trends.R
@@ -1,8 +1,8 @@
 # PROJECT:  Starting Projects
 # AUTHOR:   P. Ranade | USAID
-# PURPOSE:  New and current treatment trends over time
+# PURPOSE:  Historic TX trends
 # LICENSE:  MIT
-# DATE:     2023-03-09
+# DATE:     2023-03-14
 
 # DEPENDENCIES ------------------------------------------------------------
 
@@ -20,60 +20,62 @@ library(glue)
 library(gophr)
 library(janitor)
 library(lubridate)
+library(gt)
 
-#setting filepath for fonts
-# font_import(paths = "C:/Users/STAR/AppData/Local/Microsoft/Windows/Fonts")
-# library(extrafont)
-# fonts()
+# DATIM GENIE REPORT PARAMETERS -------------------------------------------
+
+# PSNU By IM
+# DATIM data as of: 2/10/2023, 23:01:05 UTC
+# Genie report updated: 2/13/2023, 18:12:54 UTC
+# Current period(s): 2020 Target, 2021 Target, 2021 Q1, 2021 Q2, 2021 Q3, 2021 Q4, 2022 Target, 2022 Q1, 2022 Q2, 2022 Q3, 2022 Q4, 2023 Target, 2023 Q1
+# Daily/Frozen: Frozen
+# Indicator: TX_CURR, TX_NEW
+# Standardized Disaggregate: Total Numerator
+# Fiscal Years: 2015-2023
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
-# authors <- c("Prasann Ranade", "Nada Petrovic", "Karishma Srikanth")
+# authors <- c("Prasann Ranade", "Nada Petrovic", "Karishma Srikanth", "Aaron Chafetz")
 # ref_id <- 00000
+
+# GLOBAL VARIABLES --------------------------------------------------------
+
+genie_path <- "C:/Users/STAR/Documents/Data/Genie_PSNU_IM_Global_Frozen_0151da20-0688-4a13-a4d0-857ba8703dbc.txt" 
+#id for adorning to plots, making it easier to find on GH
+ref_id <- "bfbd380f" 
+
+#list of MSD metadata elements
+get_metadata(genie_path) 
+metadata$curr_fy
+
 
 # IMPORT ------------------------------------------------------------------
 
-df <- si_path() %>% 
-  return_latest("OU_IM_FY21-23") %>% 
-  read_psd()  
-df_arch <- si_path() %>% 
-  return_latest("OU_IM_FY15-20") %>%
-  read_psd()   
+df_tx <- read_psd(genie_path)   
 
 # MUNGE -------------------------------------------------------------------
 
-#get dates and source info
-#curr_pd <- identifypd(df)
-#curr_fy <- identifypd(df, "year")
-msd_source <- source_info()
+#filter out Military SNUs
+df_agg <- df_tx %>% 
+  filter(str_detect(snu1, "_Mil", negate = TRUE)) %>%
+  count(fiscal_year, indicator, wt = cumulative) 
 
-#get TX_CURR and TX_NEW numbers by year 
-df_tx <- df %>% 
-  #resolve_knownissues %>% 
-  bind_rows(df_arch) %>% 
-  filter(indicator %in% c("TX_CURR", "TX_NEW"),
-         standardizeddisaggregate == "Total Numerator",
-         fiscal_year <= 2021) %>% 
-  group_by(fiscal_year, indicator) %>% 
-  summarise(across(cumulative, sum, na.rm = TRUE)) %>% 
-  ungroup()
+#verify TX numbers
+df_agg %>% 
+  pivot_wider(names_from = fiscal_year, values_from = n) %>%
+  gt() %>% 
+  fmt_number(
+    columns = -indicator,
+    decimals = 2,
+    suffixing = TRUE
+  )
 
-#add cumulative year and source columns
-df_tx <- df_tx %>% 
-  rename(period = fiscal_year, value = cumulative) %>% 
-  mutate(period = str_replace(period, "20", "FY")) %>% 
-  arrange(indicator, period) %>% 
-  mutate(source = "MSD")
+#filter until 2021
+df_agg <- df_agg %>% 
+  filter(fiscal_year <= 2021)
 
- df_tx <- df_tx %>% 
-   mutate(
-     bar_alpha = case_when(period == 2021 & 
-                             str_detect(2021, "Q4", negate = TRUE) ~ .6,
-                                TRUE ~ 1),
-          year = glue("20{str_sub(period, 3, 4)}") %>% as.integer)
- View(df_tx)
 #add labels
-df_tx <- df_tx %>% 
+df_agg <- df_agg %>% 
   mutate(ind_label = case_when(indicator == "TX_CURR" ~ 
                                  "Currently receiving ART treatment",
                                TRUE ~ "Newly enrolled on ART treatment"))
@@ -83,38 +85,37 @@ label_scales <- scales::label_number(accuracy=0.1,
                                      scale_cut=scales::cut_short_scale())
 
 #add title info
-title_info <- df_tx %>% 
+title_info <- df_agg %>% 
   filter(indicator == "TX_CURR",
-         period %in% c(min(period), 2021)) %>% 
-  select(year, value) %>% 
-  mutate(added = (value - lag(value)) %>% label_scales(),
-         yrs = year - lag(year)) %>% 
-  filter(year == 2021)
+         fiscal_year %in% c(min(fiscal_year), 2021)) %>% 
+  select(fiscal_year, n) %>% 
+  mutate(added = (n - lag(n)) %>% label_scales(),
+         yrs = fiscal_year - lag(fiscal_year)) %>% 
+  filter(fiscal_year == 2021)
 
 #pull out current data point for context
-val_2021 <- df_tx %>% 
-  filter(year == 2021, indicator == "TX_CURR") %>%
-  select(indicator, value) %>% 
-  pull(value) %>% 
+val_2021 <- df_agg %>% 
+  filter(fiscal_year == 2021, indicator == "TX_CURR") %>%
+  select(indicator, n) %>% 
+  pull(n) %>% 
   label_scales()
 
-df_viz <- df_tx %>% 
-  ggplot(aes(year, value)) +
-  geom_col(aes(alpha = bar_alpha, fill = ind_label), position = "identity") +
-  #geom_hline(yintercept = seq(2e6, 6e6, 2e6), color = "white") + 
+df_viz <- df_agg %>% 
+  ggplot(aes(fiscal_year, n)) +
+  geom_col(aes(fill = ind_label), position = "identity") +
   scale_y_continuous(labels = label_number_si(),
                      position = "right", expand = c(.005, .005)) +
   scale_x_continuous(expand = c(.005, .005),
-                     n.breaks = unique(df_tx$period) %>% length()) +
-  geom_text(aes(label = label_scales(value), vjust = -0.3,
-                 family = "Source Sans Pro")) +
-  expand_limits(y = c(0,25000000)) +
+                     n.breaks = unique(df_agg$fiscal_year) %>% length()) +
+  geom_text(aes(label = label_scales(n), vjust = -0.3,
+                family = "Source Sans Pro")) +
+  expand_limits(y = c(0,20000000)) +
   scale_fill_manual(values = c(denim_light, denim)) +
   scale_alpha_identity()  +
   labs(x = NULL, y = NULL, fill = NULL,
        title = glue("AS OF 2021, PEPFAR AGENCIES HAVE PROVIDED TREATMENT FOR {val_2021} PATIENTS"),
        #subtitle = glue("OVER THE PAST {title_info$yrs} YEARS, USAID HAS ADDED {title_info$added} PATIENTS ONTO TREATMENT"),
-       caption = glue("Source: {msd_source} (including FY15-18)")) +
+       caption = glue("Source: MER {metadata$curr_fy_lab}")) +
   si_style_ygrid()
 
 plot(df_viz)
@@ -124,5 +125,4 @@ si_save("Images/fo_tx_trends_usaid.png")
 si_save("Graphics/fo_tx_trends_usaid.svg")
 
 
-  
-  
+
