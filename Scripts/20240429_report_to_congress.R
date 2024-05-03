c24b7b5d54beecdad3b3993d1ad6cf454a5d36a9
Karishma Srikanth <84093378+karishmas26@users.noreply.github.com>
2024-05-03 17:09:54 -0400

    added viz script

--- Scripts/20240429_report_to_congress.R
+++ Scripts/20240429_report_to_congress.R
@@ -3,7 +3,7 @@
 # REF ID:   614cf6d6 
 # LICENSE:  MIT
 # DATE:     2024-04-29
-# UPDATED: 
+# UPDATED:  202-05-03 (added visual)
 
 # DEPENDENCIES ------------------------------------------------------------
   
@@ -94,11 +94,19 @@
     tab_header(
       title = glue("Method 1: TX_CURR" %>% toupper()),
       subtitle = "Normal PEPFAR TX_CURR totals - exclude UKR")
+  
+  
+  df_msd_total %>% 
+    filter(fiscal_year %in% c(2022),
+           #operatingunit != "Ukraine",
+           indicator %in% c("TX_CURR")) %>% 
+    group_by(fiscal_year, indicator, standardizeddisaggregate) %>% 
+    summarise(cumulative = sum(cumulative, na.rm = TRUE), .groups = "drop")
     
   #method 2
   #filter out Military SNUs
   df_agg <- df_genie %>% 
-    filter(str_detect(snu1, "_Mil", negate = TRUE)) %>%
+   # filter(str_detect(snu1, "_Mil", negate = TRUE)) %>%
     count(fiscal_year, indicator, wt = cumulative) 
   
   #verify TX numbers
@@ -119,7 +127,6 @@
 
 # HRH -------------------------------------------------------------------
   
-  #difficult to replicate the 129k figure from report
   df_hrh %>% 
     filter(operating_unit != "Ukraine",
            program == "C&T") %>% 
@@ -135,23 +142,80 @@
   
 # BUDGET/ER ----------------------------------------------------------------
   
-  #difficulty replicating figure here too
-  df_fsd %>% 
-    filter(program == "C&T",
-     # sub_program == "C&T: HIV Drugs",
-           fiscal_year == 2021) %>% 
-    group_by(fiscal_year, program, sub_program) %>% 
-    summarise(expenditure_amt  = sum(expenditure_amt , na.rm = TRUE), .groups = "drop") %>% 
-    gt() %>% 
-    fmt_currency(
-      columns = c(4),
-      decimals = 0
-    ) %>% 
-    tab_header(
-      title = glue("HIV Procurements" %>% toupper()),
-      subtitle = "Filtered to sub_program C&T: HIV Drugs")
+  #ended up not using FSD values and instead used Pano Dossiers to calculate
+
+    # df_fsd %>% 
+    # remove_mo() %>% 
+    # filter(program == "C&T",
+    #       # operatingunit != "Ukraine",
+    #  # sub_program == "C&T: HIV Drugs",
+    #        fiscal_year == 2021) %>% 
+    # group_by(fiscal_year, planning_cycle, program, sub_program) %>% 
+    # summarise(cop_budget_total  = sum(cop_budget_total , na.rm = TRUE), .groups = "drop") %>% 
+    # gt() %>% 
+    # fmt_currency(
+    #   columns = c(5),
+    #   decimals = 0
+    # ) %>% 
+    # tab_header(
+    #   title = glue("HIV Procurements" %>% toupper()),
+    #   subtitle = "Filtered to sub_program C&T: HIV Drugs")
     
   
   
+# TX_CURR and NEW viz ----------------------------------------------------
   
+df_viz <- df_msd_total %>% 
+    filter(fiscal_year <= 2022,
+           operatingunit != "Ukraine",
+           indicator %in% c("TX_CURR", "TX_NEW"),
+           standardizeddisaggregate == "Total Numerator") %>% 
+    group_by(fiscal_year, indicator) %>% 
+    summarise(cumulative = sum(cumulative, na.rm = TRUE), .groups = "drop") %>% 
+  mutate(ind_label = case_when(indicator == "TX_CURR" ~ 
+                                 "Currently receiving ART treatment",
+                               TRUE ~ "Newly enrolled on ART treatment"))
+
+
+#shorten numbers to K/M
+label_scales <- scales::label_number(accuracy=0.1, 
+                                     scale_cut=scales::cut_short_scale())
+
+#add title info
+title_info <- df_viz %>% 
+  filter(indicator == "TX_CURR",
+         fiscal_year %in% c(min(fiscal_year), max(fiscal_year))) %>% 
+  select(fiscal_year, cumulative) %>% 
+  mutate(added = (cumulative - lag(cumulative)) %>% label_scales(),
+         yrs = fiscal_year - lag(fiscal_year)) %>% 
+  filter(fiscal_year == max(fiscal_year))
+
+#pull out current data point for context
+val_2022 <- df_viz %>% 
+  filter(fiscal_year == 2022, indicator == "TX_CURR") %>%
+  select(indicator, cumulative) %>% 
+  pull(cumulative) %>% 
+  label_scales()
+
+df_viz %>% 
+  mutate(viz_alpha = ifelse(fiscal_year == max(fiscal_year), 0.8, 0.6)) %>% 
+  ggplot(aes(fiscal_year, cumulative, alpha = viz_alpha)) +
+  geom_col(aes(fill = ind_label), position = "identity") +
+  scale_y_continuous(labels = label_number(scale_cut = cut_short_scale()),
+                     position = "right", expand = c(.005, .005)) +
+  scale_x_continuous(expand = c(.005, .005),
+                     n.breaks = unique(df_viz$fiscal_year) %>% length()) +
+  geom_text(aes(label = label_scales(cumulative), vjust = -0.3,
+                family = "Source Sans Pro")) +
+  expand_limits(y = c(0,22000000)) +
+  scale_fill_manual(values = c(denim_light, "#5378A5")) +
+  scale_alpha_identity()+
+  labs(x = NULL, y = NULL, fill = NULL,
+       title = glue("AS OF 2021, PEPFAR AGENCIES HAVE PROVIDED TREATMENT FOR {val_2022} PATIENTS"),
+       #subtitle = glue("OVER THE PAST {title_info$yrs} YEARS, USAID HAS ADDED {title_info$added} PATIENTS ONTO TREATMENT"),
+       caption = glue("Note: Ukraine removed from PEPFAR totals
+                      Source: {metadata$source} | US Agency for International Development")) +
+  si_style_ygrid()
+
+si_save("Graphics/20240429_txcurr_congress_report.svg")
     
