918eba250c7dcf2e3c052294a56ede0718dea287
Aaron Chafetz <achafetz@usaid.gov>
2024-06-05 14:05:02 -0400

    updated gh plots

--- Scripts/20240604_GH-ResultFrameworkDomains.R
+++ Scripts/20240604_GH-ResultFrameworkDomains.R
@@ -1,10 +1,10 @@
-# PROJECT:  
-# PURPOSE:  
+# PROJECT:  catch-22
+# PURPOSE:  provide a sense of where indicators sit on maturity matrix
 # AUTHOR:   A.Chafetz | USAID
 # REF ID:   caeb7159 
 # LICENSE:  MIT
 # DATE:     2024-06-04
-# UPDATED: 
+# UPDATED:  2024-06-05
 
 # DEPENDENCIES ------------------------------------------------------------
   
@@ -25,22 +25,30 @@
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
+  ref_id <- "caeb7159"
+    
   load_secrets("email")  
 
   gs_id <- as_sheets_id("1O4-c1lXeL21PzmaiuLqHxPapnoktsIykmln6cs1P70E") 
   
+  gs_sht <- "Data Revised"
   
 # IMPORT ------------------------------------------------------------------
   
-  df_rfd <- read_sheet(gs_id,col_types = "c")
+  df_rfd <- read_sheet(gs_id, gs_sht, col_types = "c")
   
 
 # MUNGE -------------------------------------------------------------------
 
-  df_long <- df_rfd %>% 
+  df_rfd_lim <- df_rfd %>% 
+    select(-contains("Institutionalized "), -Total) %>% glimpse()
+  
+  df_long <- df_rfd_lim %>% 
     pivot_longer(-starts_with("rf"),
-                names_to = "status",
-                values_to = "count") %>% 
+                 names_to = "status",
+                 values_to = "count") %>% 
+    mutate(is_approved = str_detect(status, "Not Approved", negate = TRUE),
+           status_alt = str_remove(status, " -.*")) %>% 
     group_by(rf_num) %>% 
     mutate(count = as.double(count),
            count_rf = sum(count) %>% as.character(),
@@ -51,32 +59,43 @@
                                 "Access to Services through Health Systems Enhanced" ~ "Access to Services\nthrough Health\nSystems Enhanced",
                                 "Resilient Ecosystems Sustained" ~ "Resilient Ecosystems\nSustained",
                                 "Knowledge and Innovative Solutions" ~ "Knowledge and\nInnovative Solutions"
-           )) %>% 
-    mutate(rf_desc = glue("{rf_num}:\n{rf_name}\n{count_lab}")) %>% 
-    # unite(rf_desc, c(rf_num, rf_name), sep = ":\n") %>% 
+    )) %>% 
+    mutate(rf_desc = glue("{rf_num}:\n{rf_name}\n{count_lab}"),
+           border_color = case_when(rf_ind_cat == "PPR" ~ hw_electric_indigo,
+                                    rf_ind_cat == "SDG" ~ hw_orchid_bloom,
+                                    TRUE ~ hw_slate),
+           fill_color = ifelse(is_approved == FALSE, NA_character_, border_color)) %>% 
     mutate(status = status %>% 
              str_replace(" - | ", "\n") %>% 
              as_factor(),
+           status_alt = as_factor(status_alt),
            rf_desc= as_factor(rf_desc) %>% fct_rev) %>% 
-    filter(count > 0)
+    filter(count > 0,
+           !is.na(rf_name))
 
   df_points <- df_long %>% 
     uncount(count)
   
 
+  df_long %>% 
+    count(status, wt = count)
+  
 # VIZ ---------------------------------------------------------------------
   
-  
   df_points %>% 
-    ggplot(aes(status, rf_desc)) + 
-    geom_point(aes(color = fct_rev(rf_ind_cat)),
+    ggplot(aes(status_alt, rf_desc)) + 
+    geom_point(aes(fill = fill_color, 
+                   color = border_color),
+               shape = 21,
                position = position_jitter(width = .3, height = .3, seed = 42),
                size = 4, alpha = .6) +
     scale_x_discrete(position = "top") +
-    scale_color_manual(values = c(hw_orchid_bloom, hw_electric_indigo, hw_slate)) +
-    labs(x = NULL, y = NULL, color = NULL,
+    scale_fill_identity() +
+    scale_color_identity() +
+    labs(x = NULL, y = NULL, fill = NULL, color = NULL,
          # title = "Mapping Indicators to GH Results Framework Domains"
-         ) +
+         caption = glue("USAID/GH | {Sys.Date()} | Ref id: {ref_id}")
+    ) +
     si_style_nolines() +
     # si_style_nolines(font_title = "Gill Sans MT",
     #                  font_subtitle = "Gill Sans MT",
@@ -85,6 +104,11 @@
     theme(legend.position = "none")
 
   si_preview()
-  si_save("../Downloads/gh_rf_domains.svg",
-          height = 4.75)
+  # si_save("Graphics/gh_rf_domains.svg", height = 4.75)
+  si_save("Graphics/gh_rf_domains.svg",
+          height = 4, width = 8.5)
+  
+  
+  
+ 
   
