dea5b5a55a52e0d09bd10dbff921cabf97925ad6
Karishma Srikanth <84093378+karishmas26@users.noreply.github.com>
2024-06-13 10:49:47 -0400

    initialize script

---
+++ Scripts/2024 GH Portfolio Review/20240613_ART_cov_maps_trends.R
@@ -0,0 +1,27 @@
+# AUTHOR:   K. Srikanth | USAID
+# PURPOSE:  PEPFAR SSA Progress to 2nd 95 Map
+# REF ID:   aae5029f 
+# LICENSE:  MIT
+# DATE:     2024-06-13
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  sipack
+
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  # SI specific paths/functions  
+    load_secrets()
+
+  # Grab metadata
+    get_metadata(filepath) 
+  
+  ref_id <- "aae5029f"
+
+# IMPORT ------------------------------------------------------------------
+  
+  return_data
+  msd
+
+# MUNGE -------------------------------------------------------------------
