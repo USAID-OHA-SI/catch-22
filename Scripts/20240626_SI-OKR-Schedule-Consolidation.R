d3077103da3e39c044f220769574df8d93db610d
Aaron Chafetz <achafetz@usaid.gov>
2024-07-17 12:12:00 -0400

    update plot

--- Scripts/20240626_SI-OKR-Schedule-Consolidation.R
+++ Scripts/20240626_SI-OKR-Schedule-Consolidation.R
@@ -15,6 +15,7 @@
   library(glamr)
   library(cli)
   library(glitr)
+  library(ggtext)
 
   
 # GLOBAL VARIABLES --------------------------------------------------------
@@ -93,30 +94,67 @@
 
   df_okr_13 <- df_sch %>% 
     filter(okr_number == 1.3)
-
-  df_okr_13 <- df_okr_13 %>% 
-    mutate(task_disp = ifelse(str_length(task) <= 20, task, str_glue("{str_sub(task, end = 20)}...")),
-           task_disp = task_disp %>% fct_inorder() %>% fct_rev())
   
   df_okr_13 <- df_okr_13 %>% 
-    select(task, task_disp, status,
+    mutate(responsible_initials = map_chr(responsible, \(x) {x %>% 
+        str_extract_all("^[:upper:]|(?<= )[:upper:]") %>%
+        unlist() %>% 
+        paste0(collapse = '')
+    }),
+    collaborators_initials = map_chr(collaborators, \(x) {
+      x %>% 
+        str_extract_all("^[:upper:]|(?<= )[:upper:]") %>%
+        unlist() %>% 
+        paste0(collapse = '') %>% 
+        gsub("(.{2})", "\\1,", .) %>% 
+        str_remove(",$")
+    }),
+    collaborators_initials = ifelse(collaborators_initials != "NA", 
+                                    str_glue('+{collaborators_initials}'),
+                                    ''),
+    workers = str_glue("{responsible_initials}{collaborators_initials}"))
+  
+  df_okr_13 <- df_okr_13 %>% 
+    mutate(okr = str_glue("{unique(df_okr_13$okr_number)} {unique(df_okr_13$okr)}"),
+           task_disp = ifelse(str_length(task) <= 20, task, str_glue("{str_sub(task, end = 20)}...")),
+           task_disp = str_glue("{task_disp}<br>_{workers}_"),
+           task_disp = task_disp %>% fct_inorder() %>% fct_rev())
+  
+  
+  
+  df_okr_13 <- df_okr_13 %>% 
+    select(okr, task, task_disp, status,
            target_start_date, target_end_date = anticipated_end_date,
            actual_start_date, actual_end_date) %>% 
+    mutate(status = case_when(is.na(status) & !is.na(actual_end_date) ~ "Complete",
+                              !is.na(status) ~ status,
+                              is.na(actual_start_date) & target_end_date < today() ~ "Behind",
+                              TRUE ~ "Planned"),
+           actual_start_date = ifelse(status == "Behind" | (target_start_date < today() & status != "Completed"), today(), actual_start_date) %>% as_date)
+           # actual_start_date = ifelse(status == "Behind", today(), actual_start_date) %>% as_date)
+
+  df_okr_13 <- df_okr_13 %>% 
     pivot_longer(ends_with("date"),
                  names_to = c("type", ".value"),
                  names_pattern = "(.*)_(.*)_date",
                  values_to = "date")
   
-  df_okr_13 <- df_okr_13 %>% 
-    mutate(pt_shp = case_when(status == "In progress" ~ 8,
+  df_viz <- df_okr_13 %>% 
+    mutate(pt_shp = case_when(type == "actual" & status %in% c("In progress", "Behind") ~ 8,
+                              type == "actual" & status != "Completed" & start <= today() & is.na(end)  ~ 8,
                               TRUE ~ 21),
-           fill_color = case_when(type == "target" & status == "Completed" ~ slate,
-                                  type == "target" & is.na(end) ~ orchid_bloom,
+           fill_color = case_when(type == "target" & status == "Completed" ~ si_palettes$slate_t[3],
+                                  type == "target"  & status == "Behind" ~ orchid_bloom,
                                   type == "target" ~ slate,
+                                  type == "actual" & status == "Completed" ~ si_palettes$electric_indigo_t[2],
                                   TRUE ~ electric_indigo
-                                  ))
+                                  ),
+           task_disp = ifelse(status %in% c("In progress", "Behind"), 
+                              str_glue("**{task_disp}**"), str_glue("{task_disp}")) %>% fct_inorder() %>% fct_rev())
 
-  df_okr_13 %>% 
+  v_okr_name <- unique(df_viz$okr)
+  
+  df_viz %>% 
     ggplot(aes(y = task_disp, group = type, color = fill_color, fill = fill_color)) +
     geom_vline(xintercept = Sys.Date(), color = tango) +
     geom_segment(aes(start, xend = end), na.rm = TRUE,
@@ -127,10 +165,11 @@
     geom_point(aes(end, shape = pt_shp), na.rm = TRUE,
                stroke = 1.1,
                position = position_dodge(width = .75)) +
-    scale_x_date(position = "top") +
     scale_fill_identity() +
     scale_color_identity() +
     scale_shape_identity() +
     labs(x = NULL, y = NULL,
+         title = str_wrap(v_okr_name),
          caption = str_glue("Source: SI Branch PM Processes & Tools [accessed {today()}] | Ref Id: {ref_id}")) +
-    si_style()
+    si_style() +
+    theme(axis.text.y = element_markdown())
